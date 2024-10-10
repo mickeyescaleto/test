05f933000670cbf58b2f8ef000f0d09bc6941d2f
mickeyescaleto <mickeyescaleto@gmail.com>
2024-10-11 00:17:38 +0300

    feat: add cors and swagger for the elysia app

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,5 +1,9 @@
 import { Elysia } from 'elysia';
+import { cors } from '@elysiajs/cors';
+import { swagger } from '@elysiajs/swagger';
 
 const app = new Elysia()
+  .use(cors())
+  .use(swagger())
   .get('/', () => 'Hello Elysia')
   .listen(3000, ({ url }) => console.log(`🦊 Elysia is running at ${url}`));
